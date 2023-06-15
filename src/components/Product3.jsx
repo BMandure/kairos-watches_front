68bc09bc43f7fdcfe5eb0507acc66e0800ba29f0
G-Laffitte <gastonlaffittem@gmail.com>
2023-06-15 16:23:19 -0300

    Product3
    Product3

---
+++ src/components/Product3.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Product3.css";
+
+function Product3() {
+  return <div>hola</div>;
+}
+
+export default Product3;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Product2 from "../components/Product2";
+import Product3 from "../components/Product3";
 
 function Home() {
   return (
     <div>
       <Product2 />
+      <Product3 />
     </div>
   );
 }
