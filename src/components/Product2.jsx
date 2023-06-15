bf4660f37dbef5676491e5dd49f7561b3dad85c3
G-Laffitte <gastonlaffittem@gmail.com>
2023-06-15 16:12:28 -0300

    Update Product 2
    update product 2

--- src/components/Product2.jsx
+++ src/components/Product2.jsx
@@ -10,79 +10,76 @@ import img6 from "../assets/oysterGold.png";
 
 function Product2() {
   return (
-    <div className="about-intro-container-product2">
-      <Container className="">
-        <Row className="about-row">
-          <Col xs={7} className="about-col d-flex flex-column">
-            <h2 className="about-intro-title">
-              Rolex{" "}
-              <img
-                src="../src/assets/rolexcrown.png"
-                alt="RolexCrown"
-                id="product2-rolex-crown"
-              ></img>
-            </h2>
-            <p className="about-intro-text">
-              Rolex stands as an emblem of timeless sophistication, with the
-              Submariner as its crowning jewel. From its commitment to
-              innovation in watchmaking to its partnerships in the realm of
-              sports, Rolex continues to inspire with its unwavering pursuit of
-              excellence.
-            </p>
+    <Container id="about-intro-container-product2">
+      <Row className="about-row">
+        <Col xs={7} className="about-col-product2 d-flex flex-column">
+          <h2 className="about-intro-title-product2">
+            Rolex{" "}
+            <img
+              src="../src/assets/rolexcrown.png"
+              alt="RolexCrown"
+              id="product2-rolex-crown"
+            ></img>
+          </h2>
+          <p className="about-intro-text-product2">
+            Rolex stands as an emblem of timeless sophistication, with the
+            Submariner as its crowning jewel. From its commitment to innovation
+            in watchmaking to its partnerships in the realm of sports, Rolex
+            continues to inspire with its unwavering pursuit of excellence.
+          </p>
 
-            <div>
-              <button>View collections</button>
-            </div>
-          </Col>
-          <Col xs={5} className="about-col">
-            <Carousel fade controls={false} indicators={false}>
-              <Carousel.Item>
-                <img
-                  className="about-intro-img"
-                  src={img}
-                  alt="about the project intro"
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="about-intro-img"
-                  src={img2}
-                  alt="about the project intro"
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="about-intro-img"
-                  src={img3}
-                  alt="about the project intro"
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="about-intro-img"
-                  src={img4}
-                  alt="about the project intro"
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="about-intro-img"
-                  src={img5}
-                  alt="about the project intro"
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="about-intro-img"
-                  src={img6}
-                  alt="about the project intro"
-                />
-              </Carousel.Item>
-            </Carousel>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+          <div>
+            <button>View collections</button>
+          </div>
+        </Col>
+        <Col xs={5} className="about-col-product2">
+          <Carousel fade controls={false} indicators={false}>
+            <Carousel.Item>
+              <img
+                className="about-intro-img-product2"
+                src={img}
+                alt="about the project intro"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="about-intro-img-product2"
+                src={img2}
+                alt="about the project intro"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="about-intro-img-product2"
+                src={img3}
+                alt="about the project intro"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="about-intro-img-product2"
+                src={img4}
+                alt="about the project intro"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="about-intro-img-product2"
+                src={img5}
+                alt="about the project intro"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="about-intro-img-product2"
+                src={img6}
+                alt="about the project intro"
+              />
+            </Carousel.Item>
+          </Carousel>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
