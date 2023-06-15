486a6fc9c82b7411faa57966f3a8b8d09d281287
Bruno Manduré <brunomanduremedina99@gmail.com>
2023-06-15 17:49:44 -0300

    product1

--- src/components/Product1.jsx
+++ src/components/Product1.jsx
@@ -1,7 +1,16 @@
 import "./Product1.css";
 
 function Product1() {
-  return <div>Product1</div>;
+  return (
+    <div className="product1-container">
+      <div className="product1-background">
+        <div className="product1-title-container">
+          <h2 className="product1-title">Patek Philippe</h2>
+          <button className="product1-button">View collections</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Product1;
