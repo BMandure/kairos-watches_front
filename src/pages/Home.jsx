1d75052f6f2327f5cd5a85de20e840d68092d59f
Bruno Manduré <brunomanduremedina99@gmail.com>
2023-06-15 19:30:45 -0300

    Revert "Product 3"
    This reverts commit c4de0cb419503532a79f87f8f62971b7b7e2bd4f.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Product3 from "../components/Product3";
 import Product2 from "../components/Product2";
 import Product1 from "../components/Product1";
 
@@ -8,7 +7,6 @@ function Home() {
     <div>
       <Product1 />
       <Product2 />
-      <Product3 />
     </div>
   );
 }
