08fa3b4aa69905f439632e1dda7b25904a5e3806
G-Laffitte <gastonlaffittem@gmail.com>
2023-06-16 12:04:13 -0300

    Patek done
    Component Patek done with videos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,6 +16,19 @@ import victorinox2 from "../assets/maverick.mp4";
 import victorinox1 from "../assets/INOX.mp4";
 import victorinoxLogo from "../assets/victorinoxLogo.png";
 
+import omega1 from "../assets/omegaVintage.jpeg";
+import omega2 from "../assets/omegaGreen.jpeg";
+import omega3 from "../assets/omegaRedBlue.jpeg";
+import omega4 from "../assets/omegaDark.jpeg";
+import omega5 from "../assets/about3.jpg";
+import omega6 from "../assets/omegaVintage.jpeg";
+
+import omegaLogo from "../assets/omegaLogo.png";
+
+import patekLogo from "../assets/patekLogo.png";
+import patek1 from "../assets/patek1.mp4";
+import patek2 from "../assets/patek2.mp4";
+
 function Home() {
   const imgRolex = [rolex1, rolex2, rolex3, rolex4, rolex5, rolex6];
   const descRolex =
@@ -24,6 +37,16 @@ function Home() {
   const victorinoxVideos = [victorinox1, victorinox2];
   const descVictorinox =
     "Victorinox Swiss Army stands as a symbol of timeless sophistication, with its remarkable timepieces capturing the essence of enduring elegance. From their commitment to innovation in watchmaking to their dedication to precision and functionality, Victorinox Swiss Army watches inspire with their unwavering pursuit of excellence!";
+
+  const imgOmega = [omega1, omega2, omega3, omega4, omega5, omega6];
+
+  const descOmega =
+    "Omega, a true icon of refined sophistication, embodies unparalleled elegance and prestige, with the Seamaster proudly standing as its crowning achievement. Through an unwavering commitment to pushing the boundaries of horological craftsmanship and forging extraordinary partnerships in the realm of athletics, Omega stands as a timeless source of inspiration, ceaselessly pursuing perfection.";
+
+  const descPatek =
+    "Patek Philippe exudes an aura of timeless elegance, with the Calatrava as its dazzling masterpiece. From its unwavering dedication to pioneering craftsmanship to its prestigious collaborations in the world of horology, Patek Philippe remains an unrivaled source of inspiration, ceaselessly striving for perfection.";
+
+  const patekVideos = [patek1, patek2];
 
   return (
     <div>
@@ -41,16 +64,17 @@ function Home() {
         videos={victorinoxVideos}
       />
       <Product2
-        name="Patek Philippe"
-        description={descRolex}
-        imgs={imgRolex}
-        logo={rolexLogo}
+        name="Omega"
+        description={descOmega}
+        imgs={imgOmega}
+        logo={omegaLogo}
       />
       <Product3
-        name="Omega"
-        description={descVictorinox}
-        logo={victorinoxLogo}
-        videos={victorinoxVideos}
+        name="Patek Philippe"
+        description={descPatek}
+        logo={patekLogo}
+        videos={patekVideos}
+        className="patek-product"
       />
     </div>
   );
