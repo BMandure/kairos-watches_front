147aec485387f461677aebb02305162ba352494a
Bruno Manduré <brunomanduremedina99@gmail.com>
2023-06-16 09:39:36 -0300

    home products basic

--- src/components/Product2.jsx
+++ src/components/Product2.jsx
@@ -1,32 +1,16 @@
-import React from "react";
 import { Container, Row, Col, Carousel } from "react-bootstrap";
 import "./Product2.css";
-import img from "../assets/Datejust-everose.png";
-import img2 from "../assets/submarinerGreen.jpeg";
-import img3 from "../assets/watch-city.jpeg";
-import img4 from "../assets/batman.jpeg";
-import img5 from "../assets/about5.jpg";
-import img6 from "../assets/oysterGold.png";
 
-function Product2() {
+function Product2({ name, logo, description, imgs }) {
   return (
     <Container id="about-intro-container-product2">
       <Row className="about-row">
         <Col xs={7} className="about-col-product2 d-flex flex-column">
           <h2 className="about-intro-title-product2">
-            Rolex{" "}
-            <img
-              src="../src/assets/rolexcrown.png"
-              alt="RolexCrown"
-              id="product2-rolex-crown"
-            ></img>
+            {name}
+            <img src={logo} alt={`${name} logo`} id="product2-logo"></img>
           </h2>
-          <p className="about-intro-text-product2">
-            Rolex stands as an emblem of timeless sophistication, with the
-            Submariner as its crowning jewel. From its commitment to innovation
-            in watchmaking to its partnerships in the realm of sports, Rolex
-            continues to inspire with its unwavering pursuit of excellence.
-          </p>
+          <p className="about-intro-text-product2">{description}</p>
 
           <div>
             <button>View collections</button>
@@ -34,48 +18,15 @@ function Product2() {
         </Col>
         <Col xs={5} className="about-col-product2">
           <Carousel fade controls={false} indicators={false}>
-            <Carousel.Item>
-              <img
-                className="about-intro-img-product2"
-                src={img}
-                alt="about the project intro"
-              />
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="about-intro-img-product2"
-                src={img2}
-                alt="about the project intro"
-              />
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="about-intro-img-product2"
-                src={img3}
-                alt="about the project intro"
-              />
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="about-intro-img-product2"
-                src={img4}
-                alt="about the project intro"
-              />
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="about-intro-img-product2"
-                src={img5}
-                alt="about the project intro"
-              />
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="about-intro-img-product2"
-                src={img6}
-                alt="about the project intro"
-              />
-            </Carousel.Item>
+            {imgs.map((item) => (
+              <Carousel.Item>
+                <img
+                  className="about-intro-img-product2"
+                  src={item}
+                  alt="about the project intro"
+                />
+              </Carousel.Item>
+            ))}
           </Carousel>
         </Col>
       </Row>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,55 @@ import Product3 from "../components/Product3";
 import Product2 from "../components/Product2";
 import Product1 from "../components/Product1";
 
+import rolex1 from "../assets/Datejust-everose.png";
+import rolex2 from "../assets/submarinerGreen.jpeg";
+import rolex3 from "../assets/watch-city.jpeg";
+import rolex4 from "../assets/batman.jpeg";
+import rolex5 from "../assets/about5.jpg";
+import rolex6 from "../assets/oysterGold.png";
+
+import rolexLogo from "../assets/rolexcrown.png";
+
+import victorinox2 from "../assets/maverick.mp4";
+import victorinox1 from "../assets/INOX.mp4";
+import victorinoxLogo from "../assets/victorinoxLogo.png";
+
 function Home() {
+  const imgRolex = [rolex1, rolex2, rolex3, rolex4, rolex5, rolex6];
+  const descRolex =
+    "Rolex stands as an emblem of timeless sophistication, with the Submariner as its crowning jewel. From its commitment to innovation in watchmaking to its partnerships in the realm of sports, Rolex continues to inspire with its unwavering pursuit of excellence.";
+
+  const victorinoxVideos = [victorinox1, victorinox2];
+  const descVictorinox =
+    "Victorinox Swiss Army stands as a symbol of timeless sophistication, with its remarkable timepieces capturing the essence of enduring elegance. From their commitment to innovation in watchmaking to their dedication to precision and functionality, Victorinox Swiss Army watches inspire with their unwavering pursuit of excellence!";
+
   return (
     <div>
       <Product1 />
-      <Product2 />
-      <Product3 />
+      <Product2
+        name="Rolex"
+        description={descRolex}
+        imgs={imgRolex}
+        logo={rolexLogo}
+      />
+      <Product3
+        name="Victorinox"
+        description={descVictorinox}
+        logo={victorinoxLogo}
+        videos={victorinoxVideos}
+      />
+      <Product2
+        name="Patek Philippe"
+        description={descRolex}
+        imgs={imgRolex}
+        logo={rolexLogo}
+      />
+      <Product3
+        name="Omega"
+        description={descVictorinox}
+        logo={victorinoxLogo}
+        videos={victorinoxVideos}
+      />
     </div>
   );
 }
