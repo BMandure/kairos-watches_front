7f205cc06b3fc3c178c4e6b29ab99ed6c9311d2f
Bruno Manduré <brunomanduremedina99@gmail.com>
2023-06-16 14:35:25 -0300

    productCard and one line in footer

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -14,11 +14,9 @@ function ProductCard({ product }) {
             product.image[0]
           }`}
         />
-        <Card.Body>
-          <Card.Title>{`${product.brand.name} - ${product.serie.name} ${product.name}`}</Card.Title>
-          <Link className="product-card-button" variant="primary">
-            View Product
-          </Link>
+        <Card.Body className="product-card-body">
+          <Card.Title className="card-info">{`${product.brand.name} - ${product.serie.name} ${product.name}`}</Card.Title>
+          <Link className="product-card-button">VIEW DETAILS</Link>
         </Card.Body>
       </Card>
     </>
