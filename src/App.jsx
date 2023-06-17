d9e6f0bf48c415accb5b28893470b2a19786b898
Bruno Manduré <brunomanduremedina99@gmail.com>
2023-06-17 18:11:53 -0300

    app and navbar overflow fixed

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
   return (
     <>
       <NavbarSite />
-      <div className="app-container">
+      <div>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
