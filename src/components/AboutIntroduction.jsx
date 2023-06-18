c4224311f2f2d7b9a4520249ad1f66791c4cddc7
Bruno Manduré <brunomanduremedina99@gmail.com>
2023-06-17 22:29:20 -0300

    about page classes, text 'about the project' responsive and team card links adjusted

--- src/components/AboutIntroduction.jsx
+++ src/components/AboutIntroduction.jsx
@@ -5,12 +5,12 @@ import "./AboutIntroduction.css";
 function AboutIntroduction() {
   return (
     <Container className="about-intro-banner">
+      <h2 className="about-intro-title">About The Proyect</h2>
       <Row className="about-row">
-        <Col xs={12} className="about-col">
-          <h2 className="about-intro-title">About The Proyect</h2>
+        <Col xs={12} lg={6} className="about-col">
           <div className="container-text">
             <p className="about-intro-text">
-              (Nombre de e-commerce) es un e-commerce de relojes de alta gama,
+              <strong>Kairos</strong> es un e-commerce de relojes de alta gama,
               desarrollado por un grupo de estudiantes de Hack Academy con fines
               unicamente educativos.
             </p>
@@ -21,24 +21,25 @@ function AboutIntroduction() {
               una, como grupo tomamos la decision de hacer un e-commerce de
               relojes de alta gama, esto debido a la cantidad de imagenes y
               videos de calidad que teniamos disponibles para lograr el
-              resultado deseado.{" "}
+              resultado deseado.
             </p>
-            <p className="about-intro-text">
-              La pagina que tomamos como inspiracion es la de Omega, nos parecio
-              que abarcaba muchas de las cosas que nos gustan y buscabamos en
-              nuestro sitio. El proyecto se forma por dos subproyectos: la
-              tienda por un lado, que es una Single Page Application hecha en
-              React, donde utilizamos suna store de Redux en la cual guardamos
-              el carrito y el token, por el otro el back-end conectado a la base
-              de datos hecha en MongoDB. Nuestra tienda se comunica con el
-              servidor back-end con llamadas AJAX gracias a la libreria Axios.
-              ....
-            </p>
-          </div>
-          <div>
-            <button className="btn">View MER</button>
           </div>
         </Col>
+        <Col xs={12} lg={6} className="about-col">
+          <p className="about-intro-text">
+            La pagina que tomamos como inspiracion es la de Omega, nos parecio
+            que abarcaba muchas de las cosas que nos gustan y buscabamos en
+            nuestro sitio. El proyecto se forma por dos subproyectos: la tienda
+            por un lado, que es una Single Page Application hecha en React,
+            donde utilizamos suna store de Redux en la cual guardamos el carrito
+            y el token, por el otro el back-end conectado a la base de datos
+            hecha en MongoDB. Nuestra tienda se comunica con el servidor
+            back-end con llamadas AJAX gracias a la libreria Axios. ....
+          </p>
+        </Col>
+        <div className="about-intro-btn">
+          <button className="btn">View MER</button>
+        </div>
       </Row>
     </Container>
   );
--- src/components/AboutTeam.jsx
+++ src/components/AboutTeam.jsx
@@ -8,7 +8,7 @@ import img from "../assets/cara-de-hombre-de-negocios-contra-el-fondo-blanco.png
 
 function AboutTeam() {
   return (
-    <Container className="about-intro-banner">
+    <Container className="about-team-banner">
       <h2 className="about-team-title">The Team</h2>
       <Row className="d-flex team-row pt-5 gap-5">
         <TeamCard img={img} name="Augusto" title="Full Stack Developer" />
