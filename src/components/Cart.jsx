3236067558f7eca91282a4c78f4b7f85f17cfeb5
joaquingonjua <130697832+joaquingonjua@users.noreply.github.com>
2023-06-19 12:14:03 -0300

    Cart and Buy views

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -33,7 +33,7 @@ function Cart() {
           <Offcanvas.Title>Cart</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body className="p-0">
-          <div className="overflow-auto" style={{ height: "60%" }}>
+          <div className="overflow-auto bg-black" style={{ height: "70%" }}>
             <ItemCart />
             <ItemCart />
             <ItemCart />
@@ -46,31 +46,33 @@ function Cart() {
           </div>
           <div
             className="bg-black d-flex flex-column justify-content-around align-items-center py-3"
-            style={{ height: "40%" }}
+            style={{ height: "30%" }}
           >
-            <div className="w-75 text-white">
-              <img src={truckCart} alt="" />
-              <span className="ms-3">Kairos Insured Shipping</span>
-            </div>
-            <div className="w-75 text-white">
-              <img src={creditCard} alt="" />
-              <span className="ms-3">Safe payments</span>
-            </div>
-            <div className="w-75 text-white">
-              <img src={shield} alt="" />
-              <span className="ms-3">Two year warranty</span>
-            </div>
-            <div className="w-75 text-white">
-              <img src={check} alt="" />
-              <span className="ms-3">Free shipping worldwide</span>
-            </div>
-            <Link to={"/buy"} className="text-black mx-auto">
+            <Link to={"/buy"} className="text-black mx-auto w-75">
               <div className="w-100 bg-white p-3 d-flex justify-content-center fw-bold fs-5">
                 <img src={cart} alt="" className="px-2" />
                 <span className="px-2">Checkout</span>
                 <span className="px-2">USD 1000</span>
               </div>
             </Link>
+            <div className="info-cart-container">
+              <div className="info-cart">
+                <img src={truckCart} alt="" />
+                <span className="ms-2">Kairos Insured Shipping</span>
+              </div>
+              <div className="info-cart">
+                <img src={creditCard} alt="" />
+                <span className="ms-2">Safe payments</span>
+              </div>
+              <div className="info-cart">
+                <img src={shield} alt="" />
+                <span className="ms-2">Two year warranty</span>
+              </div>
+              <div className="info-cart">
+                <img src={check} alt="" />
+                <span className="ms-2">Free shipping worldwide</span>
+              </div>
+            </div>
           </div>
         </Offcanvas.Body>
       </Offcanvas>
--- src/components/ItemToBuy.jsx
+++ src/components/ItemToBuy.jsx
@@ -5,47 +5,33 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { Col } from "react-bootstrap";
 
+import deleteProduct from "../assets/delete.svg";
+import rest from "../assets/rest.svg";
+import add from "../assets/add.svg";
+
 function ItemBuy() {
   return (
-    <Card
-      className="m-4"
-      style={{
-        width: "18rem",
-        backgroundColor: "transparent",
-        border: "1px solid white",
-        borderRadius: "0px",
-      }}
-    >
-      <Card.Img
-        variant="top"
-        src="https://www.omegawatches.com/media/catalog/product/cache/a5c37fddc1a529a1a44fea55d527b9a116f3738da3a2cc38006fcc613c37c391/o/m/omega-de-ville-tresor-42818396003001-l.png"
-      />
-      <Card.Body>
-        <Card.Title className="text-white">Omega Watch</Card.Title>
-        <Card.Text>
-          <div className="d-flex justify-content-between fs-5 text-white">
-            <span>1 Unidad</span>
-            <span>USD 500</span>
+    <div className="position-relative">
+      <div className="d-flex justify-content-between item-cart px-3 py-1">
+        <div className="d-flex my-auto" style={{ width: "20%" }}>
+          <div style={{ width: "100%" }} className="my-auto">
+            <img
+              src="https://www.omegawatches.com/media/catalog/product/cache/a5c37fddc1a529a1a44fea55d527b9a116f3738da3a2cc38006fcc613c37c391/o/m/omega-de-ville-tresor-42818396003001-l.png"
+              alt=""
+              className="img-fluid"
+            />
           </div>
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    /*     <div className="p-4 w-75 mx-auto my-2 item-cart-container">
-        <div className="d-flex align-items-center">
-          <img
-            src="https://www.omegawatches.com/media/catalog/product/cache/a5c37fddc1a529a1a44fea55d527b9a116f3738da3a2cc38006fcc613c37c391/o/m/omega-de-ville-tresor-42818396003001-l.png"
-            alt=""
-            className="item-cart-img"
-          />
-          <p className="ms-5 item-cart-name">
-            Victorinox Swiss Army - Maverick Large - Green and Gold
-          </p>
         </div>
-        <p className="item-price">USD 500</p>
-        <span className="btn-delete-item">
-          <img src={deleteBtn} alt="" />
-        </span>
-      </div> */
+        <div
+          className="d-flex justify-content-around my-auto"
+          style={{ width: "80%" }}
+        >
+          <p className="my-1 fs-5 text-white">Rolex Gmt II Master</p>
+          <span className="my-1 fs-5 text-white">USD 500</span>
+          <img src={deleteProduct} alt="" />
+        </div>
+      </div>
+    </div>
   );
 }
 
