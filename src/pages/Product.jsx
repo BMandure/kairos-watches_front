53af5f61740bd46140e00715f1c5faee21583859
Bruno Manduré <brunomanduremedina99@gmail.com>
2023-06-22 12:01:18 -0300

    product view dynamic

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,95 +1,151 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Product.css";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import { Carousel } from "react-bootstrap";
 
 function Product() {
+  const params = useParams();
+  const [product, setProduct] = useState();
+  const [brand, setBrand] = useState(null);
+
+  useEffect(() => {
+    const getProduct = async () => {
+      const response = await axios({
+        method: "GET",
+        url: `${import.meta.env.VITE_APP_DOMAIN}/product/${params.slug}`,
+      });
+      setProduct(response.data.product);
+    };
+    getProduct();
+  }, []);
+  const [hoverFav, setHoverFav] = useState(false);
+
   return (
-    <div className="container m-4">
-      <div className="row d-flex justify-content-center align-items-center mt-4 p-4">
-        <div className="col-12 col-lg-6 p-4">
-          <div>
-            <img className="w-100" src="../src/assets/pngegg.png" alt="..." />
-          </div>
-        </div>
-        <div className="col-12 col-lg-6 p-4">
-          <div className="bg-transparent text-light d-flex flex-column">
-            <div className="d-flex justify-content-between ">
-              <div>← Previous product</div>
-              <div>Next product →</div>
+    <>
+      {product && (
+        <div className="container product-container">
+          <div className="row d-flex justify-content-center align-items-center mt-4 p-4">
+            <div className="col-12 col-lg-6 p-4">
+              <div>
+                <Carousel indicators={true}>
+                  {product.image.map((img, i) => (
+                    <Carousel.Item key={i}>
+                      <img
+                        className="w-100"
+                        src={`${
+                          import.meta.env.VITE_APP_DOMAIN
+                        }/img/products/${img}`}
+                        alt="..."
+                      />
+                    </Carousel.Item>
+                  ))}
+                </Carousel>
+              </div>
             </div>
-            <h2 className="mt-2">Rolex - Oyster Perpetual - Man</h2>
-            <div className="list-group-flush col-5 product-list-group mt-2">
-              <h2>
-                <em>USD 1300</em>
-              </h2>
-            </div>
-            <div>
-              <div className="row mt-4">
-                <h4 className="">Select your upgrades</h4>
-                <div className="col-sm-12 col-md-3 col-lg-12 border rounded m-2">
-                  <div className="form-check mt-2 mb-2">
-                    <label className="form-check-label" for="flexRadioDefault1">
-                      <h4>Black Tropic Strap</h4>
-                    </label>
-                    <input
-                      className="form-check-input "
-                      type="radio"
-                      name="flexRadioDefault"
-                      id="flexRadioDefault1"
-                    />
-                    <div>
-                      <h5>USD 39.20</h5>
-                    </div>
-                  </div>
+            <div className="col-12 col-lg-6 p-4">
+              <div className="bg-transparent text-light d-flex flex-column">
+                <div className="d-flex justify-content-between ">
+                  <Link to={`/product/slug`} className="change-product">
+                    {" "}
+                    &larr; Previous product{" "}
+                  </Link>
+                  <Link to={`/product/slug`} className="change-product">
+                    {" "}
+                    Next product &rarr;{" "}
+                  </Link>
                 </div>
-                <div className="col-sm-12 col-md-3 col-lg-12 border rounded  my-2 mx-2">
-                  <div className="form-check mt-2 mb-2">
-                    <label className="form-check-label" for="flexRadioDefault1">
-                      <h4>Grey Tropic Strap</h4>
-                    </label>
-                    <input
-                      className="form-check-input"
-                      type="radio"
-                      name="flexRadioDefault"
-                      id="flexRadioDefault1"
-                    />
-                    <div>
-                      <h5>USD 39.20</h5>
-                    </div>
-                  </div>
+                <h2 className="mt-2">{`${product.brand.name} ${product.line.name} ${product.name} - ${product.gender}`}</h2>
+                <div className="list-group-flush col-5 product-list-group mt-2">
+                  <h2>
+                    <em>{`USD ${product.price}`}</em>
+                  </h2>
                 </div>
-                <div className="col-sm-12 col-md-3 col-lg-12 border rounded  my-2 mx-2">
-                  <div className="form-check mt-2 mb-2">
-                    <label className="form-check-label" for="flexRadioDefault1">
-                      <h4>Black Nato Strap</h4>
-                    </label>
-                    <input
-                      className="form-check-input"
-                      type="radio"
-                      name="flexRadioDefault"
-                      id="flexRadioDefault1"
-                    />
-                    <div>
-                      <h5>USD 23.20</h5>
+                <div>
+                  <div className="row mt-4">
+                    <h4 className="">Select your upgrades</h4>
+                    <div className="col-sm-12 col-md-3 col-lg-12 border rounded m-2">
+                      <div className="form-check mt-2 mb-2">
+                        <label
+                          className="form-check-label"
+                          htmlFor="flexRadioDefault1"
+                        >
+                          <h4>Black Tropic Strap</h4>
+                        </label>
+                        <input
+                          className="form-check-input "
+                          type="radio"
+                          name="flexRadioDefault"
+                          id="flexRadioDefault1"
+                        />
+                        <div>
+                          <h5>USD 39.20</h5>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-sm-12 col-md-3 col-lg-12 border rounded  my-2 mx-2">
+                      <div className="form-check mt-2 mb-2">
+                        <label
+                          className="form-check-label"
+                          htmlFor="flexRadioDefault1"
+                        >
+                          <h4>Grey Tropic Strap</h4>
+                        </label>
+                        <input
+                          className="form-check-input"
+                          type="radio"
+                          name="flexRadioDefault"
+                          id="flexRadioDefault1"
+                        />
+                        <div>
+                          <h5>USD 39.20</h5>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-sm-12 col-md-3 col-lg-12 border rounded  my-2 mx-2">
+                      <div className="form-check mt-2 mb-2">
+                        <label
+                          className="form-check-label"
+                          htmlFor="flexRadioDefault1"
+                        >
+                          <h4>Black Nato Strap</h4>
+                        </label>
+                        <input
+                          className="form-check-input"
+                          type="radio"
+                          name="flexRadioDefault"
+                          id="flexRadioDefault1"
+                        />
+                        <div>
+                          <h5>USD 23.20</h5>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-          <div className="w-100 d-flex justify-content-between mt-2 align-items-center">
-            <button
-              expand="true"
-              className="btn-preorder p-4 text-center w-100"
-            >
-              <i className="bi bi-cart3 me-2"></i>Pre-order USD 1300
-            </button>
-            <div className="ms-4">
-              <i class="bi bi-heart text-light heart"></i>
+              <div className="w-100 d-flex justify-content-between mt-2 align-items-center">
+                <button
+                  expand="true"
+                  className="btn-preorder p-4 text-center w-100"
+                >
+                  <i className="bi bi-cart3 me-2"></i>Pre-order now
+                </button>
+                <div className="ms-4">
+                  <i
+                    className={`bi ${
+                      hoverFav ? "bi-heart-fill" : "bi-heart"
+                    } text-light heart`}
+                    onMouseEnter={() => setHoverFav(true)}
+                    onMouseLeave={() => setHoverFav(false)}
+                  ></i>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
