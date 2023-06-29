398d085aeb459853ae90a38919e7b02bd8a2fc15
Bruno Manduré <brunomanduremedina99@gmail.com>
2023-06-29 14:13:39 -0300

    modal, sidemenu, userInfo

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -64,90 +64,89 @@ function UserInfo() {
 
   return (
     <>
+      <h1 className="profile-title text-white text-left w-75 mx-auto">
+        Welcome, {user.firstname}!
+      </h1>
       {userInfo && (
-        <div className="user-info-fields">
-          <Row className="form-container w-100">
-            <Col xs={6}>
+        <div className="user-info-fields w-75 pt-2 mx-auto">
+          <Row className="form-container m-0 w-100">
+            <Col xs={4}>
               <p className="form-title info-field">Full name:</p>
               <p>
                 {userInfo.firstname} {userInfo.lastname}
               </p>
-            </Col>
-            <Col xs={6}>
               <p className="form-title info-field">E-mail:</p>
               <p>{userInfo.email}</p>
-            </Col>
-            <Col xs={6}>
               <p className="form-title info-field">Address:</p>
               <p>{userInfo.address}</p>
-            </Col>
-
-            <Col xs={6}>
               <p className="form-title info-field">Phone:</p>
               <p>{userInfo.phone}</p>
             </Col>
-            <hr />
-            <p className="form-title">Change your password</p>
+            <Col xs={8}>
+              <p className="form-title">Change your password</p>
 
-            {status &&
-              (status !== "Successfully password change" ? (
-                <p className="text-center font-quicksand text-danger mt-2 mb-0">
-                  {status}
-                </p>
-              ) : (
-                <p className="text-center font-quicksand text-success mt-2 mb-0">
-                  {status}
-                </p>
-              ))}
+              {status &&
+                (status !== "Successfully password change" ? (
+                  <p className="text-center font-quicksand text-danger mt-2 mb-0">
+                    {status}
+                  </p>
+                ) : (
+                  <p className="text-center font-quicksand text-success mt-2 mb-0">
+                    {status}
+                  </p>
+                ))}
 
-            <form
-              className="form mt-0"
-              onSubmit={handleSubmit}
-              autoComplete="off"
-            >
-              <div className="input-group d-flex flex-column gap-3">
-                <div>
-                  <label htmlFor="currentpassword">
-                    Current password <span className="text-danger">*</span>
-                  </label>
-                  <input
-                    type="password"
-                    name="currentpassword"
-                    id="currentpassword"
-                    value={currentPassword}
-                    onChange={(event) => setCurrentPassword(event.target.value)}
-                  />
+              <form
+                className="form mt-0"
+                onSubmit={handleSubmit}
+                autoComplete="off"
+              >
+                <div className="input-group d-flex flex-column gap-3">
+                  <div>
+                    <label htmlFor="currentpassword">
+                      Current password <span className="text-danger">*</span>
+                    </label>
+                    <input
+                      type="password"
+                      name="currentpassword"
+                      id="currentpassword"
+                      value={currentPassword}
+                      onChange={(event) =>
+                        setCurrentPassword(event.target.value)
+                      }
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="newpassword">
+                      New password <span className="text-danger">*</span>
+                    </label>
+                    <input
+                      type="password"
+                      name="newpassword"
+                      id="newpassword"
+                      value={newPassword}
+                      onChange={(event) => setNewPassword(event.target.value)}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="password">
+                      New password confirmation{" "}
+                      <span className="text-danger">*</span>
+                    </label>
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      value={password}
+                      onChange={(event) => setPassword(event.target.value)}
+                    />
+                  </div>
                 </div>
-                <div>
-                  <label htmlFor="newpassword">
-                    New password <span className="text-danger">*</span>
-                  </label>
-                  <input
-                    type="password"
-                    name="newpassword"
-                    id="newpassword"
-                    value={newPassword}
-                    onChange={(event) => setNewPassword(event.target.value)}
-                  />
+                <div className="btn-kairos pointer mt-4" onClick={handleSubmit}>
+                  <div className="btn-kairos-content">Change Password</div>
                 </div>
-                <div>
-                  <label htmlFor="password">
-                    New password confirmation{" "}
-                    <span className="text-danger">*</span>
-                  </label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                  />
-                </div>
-              </div>
-              <div className="btn-kairos pointer mt-4" onClick={handleSubmit}>
-                <div className="btn-kairos-content">Change Password</div>
-              </div>
-            </form>
+              </form>
+            </Col>
           </Row>
         </div>
       )}
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
 import UserInfo from "../components/UserInfo";
 import "./Profile.css";
+import Orders from "../components/Orders";
 
 function Profile() {
   const user = useSelector((state) => state.user);
@@ -16,11 +17,9 @@ function Profile() {
         </Col>
         <Col xs={12} md={10} lg={10} className="px-4">
           <div>
-            <h1 className="profile-title text-white text-center">
-              Welcome, {user.firstname}!
-            </h1>
             <Routes>
               <Route path="/user-info" element={<UserInfo />} />
+              <Route path="/orders" element={<Orders />} />
             </Routes>
           </div>
         </Col>
