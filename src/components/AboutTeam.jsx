5fdcd3014cbe642bfe158e62eb6a11004b0b9fcd
Bruno Manduré <brunomanduremedina99@gmail.com>
2023-06-29 14:26:49 -0300

    bruno image

--- src/components/AboutTeam.jsx
+++ src/components/AboutTeam.jsx
@@ -4,8 +4,8 @@ import TeamCard from "./TeamCard";
 
 import "./AboutTeam.css";
 
-import img from "../assets/IMG_9221-PhotoRoom.png-PhotoRoom.png";
-import BackButton from "./BackButton";
+import maxiImg from "../assets/IMG_9221-PhotoRoom.png-PhotoRoom.png";
+import brunoImg from "../assets/fotoBruno.jpg";
 
 function AboutTeam() {
   return (
@@ -13,35 +13,35 @@ function AboutTeam() {
       <h2 className="about-team-title  p-0 px-4">Our Team</h2>
       <Row className="d-flex mt-2 px-2 justify-content-between g-4">
         <TeamCard
-          img={img}
+          img=""
           name="Augusto"
           title="Full Stack Developer"
           ghLink="https://github.com/augustoriccardi"
           ldLink="https://www.linkedin.com/in/augusto-riccardi/"
         />
         <TeamCard
-          img={img}
+          img={maxiImg}
           name="Maximiliano"
           title="Full Stack Developer"
           ghLink="https://github.com/kasenawe"
           ldLink="https://www.linkedin.com/in/maximiliano-quintana-tabarez/"
         />
         <TeamCard
-          img={img}
+          img=""
           name="Gastón"
           title="Full Stack Developer"
           ghLink="https://github.com/G-Laffitte"
           ldLink="https://www.linkedin.com/in/gastonlaffitte/"
         />
         <TeamCard
-          img={img}
+          img=""
           name="Joaquin"
           title="Full Stack Developer"
           ghLink="https://github.com/joaquingonjua"
           ldLink="https://www.linkedin.com/in/joaquingonzalezjuambeltz/"
         />
         <TeamCard
-          img={img}
+          img={brunoImg}
           name="Bruno"
           title="Full Stack Developer"
           ghLink="https://github.com/BMandure"
