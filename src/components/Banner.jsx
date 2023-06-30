c6321e38ef91ba139e69e3ffa9f6467f703fa4cd
Bruno Manduré <brunomanduremedina99@gmail.com>
2023-06-30 10:28:09 -0300

    video intro fixed

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,7 +6,10 @@ function Banner() {
     <>
       <div className="container-banner container-fluid">
         <video autoPlay loop muted className="banner-video">
-          <source src="src/assets/videoIntro.mp4" type="video/mp4" />
+          <source
+            src="https://anbfiejogykibxgfpofg.supabase.co/storage/v1/object/public/media/video/home/videoIntro.mp4"
+            type="video/mp4"
+          />
         </video>
       </div>
     </>
