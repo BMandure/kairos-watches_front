b1fdb56ccab5229cb1cb320142348bd9264d6d32
Bruno Manduré <brunomanduremedina99@gmail.com>
2023-07-04 10:51:30 -0300

    reset modal

---
+++ src/components/ResetModal.jsx
@@ -0,0 +1,49 @@
+import Modal from "react-bootstrap/Modal";
+
+function ResetModal({ handleShow, handleClose, show }) {
+  const resetDB = async () => {
+    const response = await axios({
+      method: "GET",
+      url: `${import.meta.env.VITE_API_DOMAIN}/reset`,
+    });
+    handleClose();
+  };
+  const handleReset = () => {
+    resetDB;
+    handleClose();
+  };
+  return (
+    <>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        centered
+        className="modal-content-style"
+      >
+        <Modal.Header
+          closeButton
+          closeVariant="white"
+          className="modal-content-article "
+        >
+          <Modal.Title>Welcome to Karios!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="modal-content-article p-40">
+          For a better experience you may want to reset the database.
+        </Modal.Body>
+        <Modal.Footer className="modal-content-article border-top-0">
+          <div className="btn-kairos pointer">
+            <div
+              variant="primary"
+              className="btn-kairos-content"
+              onClick={handleReset}
+            >
+              Reset Database
+            </div>
+          </div>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default ResetModal;
